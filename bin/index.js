26832c81ffa11aa2e215563383db8e72ea7d8dd0
jatinsahijwani <jatinsahijwani2151@gmail.com>
2025-11-17 14:02:36 +0530

    revamp: added scripts for vm integration

---
+++ bin/index.js
@@ -0,0 +1,12 @@
+const { compileCircuit } = require('../lib/compile');
+const { testCircuit } = require('../lib/test');
+const { deployVerifier } = require('../lib/deploy');
+const { verifyProof } = require('../lib/verify');
+
+
+module.exports = {
+    verifyProof,
+    compileCircuit,
+    testCircuit,
+    deployVerifier
+};
